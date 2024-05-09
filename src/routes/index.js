014062716728c86bfdd1abc0c432caf6819033ec
Gabriel Luiz <gabriel.araujo@ubus.com.br>
2024-05-09 13:17:18 -0300

    texto padrão

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import endereco from './enderecosRoutes.js'
 
 const routes = (app) => {
-    app.route("/").get((req, res) => res.status(200).send("Curso se Node.js"));
+    app.route("/").get((req, res) => res.status(200).send("Node.js"));
 
     app.use(express.json(), endereco);
     
